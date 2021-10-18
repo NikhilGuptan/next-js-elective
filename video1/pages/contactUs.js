efe680ed48d3cf2b6d7d674741cb686185465a58
Nikhil Gupta <ng3152340@gmail.com>
2021-10-18 08:57:28 +0530

    video 1 secoud assinment

---
+++ video1/pages/contactUs.js
@@ -0,0 +1,10 @@
+const ContactUs = () => {
+
+    return(
+        <div style={{width:"50%",height:"300px",background:"blue"}}>
+            contact Us page 
+        </div>
+    )
+}
+
+export default ContactUs;
